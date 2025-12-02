1a4d95f32cb5bd9896ca2675faf333b9a9339af4
Icha <footmouthkick@gmail.com>
2025-12-01 23:24:18 -0600

    add bot routes to api v1 prefix for discord bot endpoint organization

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     require base_path('routes/api_v1.php');
+    require __DIR__.'/bot.php';
 });
 
 // When V2 is ready:
