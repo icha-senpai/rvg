4664938e30d9436d813d16ca1c7057ede60ed4c9
Icha <footmouthkick@gmail.com>
2026-05-26 18:00:14 -0500

    Allow HorizonInput to pass field attributes

--- backend/resources/js/Components/HorizonInput.vue
+++ backend/resources/js/Components/HorizonInput.vue
@@ -1,59 +1,57 @@
 <template>
   <div class="hz-stack-sm">
-
-    <!-- Label -->
     <label v-if="label" class="hz-section-label">
       {{ label }}
     </label>
 
-    <!-- SELECT -->
     <select
       v-if="type === 'select'"
       v-model="model"
+      v-bind="$attrs"
       class="hz-input cursor-pointer"
     >
       <option
         v-for="opt in options"
-        :key="opt.value"
+        :key="String(opt.value)"
         :value="opt.value"
       >
         {{ opt.label }}
       </option>
     </select>
 
-    <!-- TEXTAREA -->
     <textarea
       v-else-if="type === 'textarea'"
       v-model="model"
+      v-bind="$attrs"
       :placeholder="placeholder"
       class="hz-textarea hz-input"
-      rows="5"
     ></textarea>
 
-    <!-- INPUT -->
     <input
       v-else
-      :type="type"
       v-model="model"
+      v-bind="$attrs"
+      :type="type"
       :placeholder="placeholder"
       class="hz-input"
     />
-
   </div>
 </template>
 
 <script setup>
-const model = defineModel();
+defineOptions({
+  inheritAttrs: false,
+})
+
+const model = defineModel()
 
 defineProps({
   label: String,
   placeholder: String,
   type: { type: String, default: 'text' },
-
-  // Only used if type === 'select'
   options: {
     type: Array,
     default: () => [],
   },
-});
+})
 </script>
