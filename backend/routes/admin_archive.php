ed7d46a69ce25eecc285ec90823fee68343e61f2
Icha <footmouthkick@gmail.com>
2026-05-26 14:34:07 -0500

    Add archive taxonomy admin routes

--- backend/routes/admin_archive.php
+++ backend/routes/admin_archive.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Web\Admin\AdminArchiveController;
 use App\Http\Controllers\Web\Admin\AdminArchiveEntryController;
+use App\Http\Controllers\Web\Admin\AdminArchiveTaxonomyController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'can:access-admin-panel'])
@@ -9,6 +10,15 @@ Route::middleware(['auth', 'can:access-admin-panel'])
     ->name('admin.archive.')
     ->group(function () {
         Route::get('/', [AdminArchiveController::class, 'index'])->name('index');
+
+        Route::get('/taxonomy', [AdminArchiveTaxonomyController::class, 'index'])->name('taxonomy.index');
+        Route::post('/taxonomy/categories', [AdminArchiveTaxonomyController::class, 'storeCategory'])->name('taxonomy.categories.store');
+        Route::put('/taxonomy/categories/{category}', [AdminArchiveTaxonomyController::class, 'updateCategory'])->name('taxonomy.categories.update');
+        Route::delete('/taxonomy/categories/{category}', [AdminArchiveTaxonomyController::class, 'destroyCategory'])->name('taxonomy.categories.destroy');
+        Route::post('/taxonomy/tags', [AdminArchiveTaxonomyController::class, 'storeTag'])->name('taxonomy.tags.store');
+        Route::put('/taxonomy/tags/{tag}', [AdminArchiveTaxonomyController::class, 'updateTag'])->name('taxonomy.tags.update');
+        Route::delete('/taxonomy/tags/{tag}', [AdminArchiveTaxonomyController::class, 'destroyTag'])->name('taxonomy.tags.destroy');
+
         Route::post('/topics', [AdminArchiveController::class, 'store'])->name('topics.store');
         Route::put('/topics/{topic}', [AdminArchiveController::class, 'update'])->name('topics.update');
         Route::delete('/topics/{topic}', [AdminArchiveController::class, 'destroy'])->name('topics.destroy');
